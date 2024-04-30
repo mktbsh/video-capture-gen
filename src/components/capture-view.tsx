4e46bffca05ad78db636bb75dcbe8bbd60d9e268
mktbsh <t93.hasebe@gmail.com>
2024-04-30 10:56:04 +0900

    feat: capture preview dialog

--- src/components/capture-view.tsx
+++ src/components/capture-view.tsx
@@ -1,6 +1,14 @@
 import { Capture } from "@/infra/db/capture";
 import { useObjectURL } from "@/hooks/use-object-url";
 
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 type Props = {
   capture: Capture;
 };
@@ -9,11 +17,26 @@ export function CaptureView({ capture }: Props) {
   const { url } = useObjectURL(capture.data);
 
   return (
-    <img
-      src={url}
-      alt=""
-      decoding="async"
-      className="w-auto h-full pointer-events-none object-cover"
-    />
+    <Dialog>
+      <DialogTrigger>
+        <img
+          src={url}
+          alt=""
+          decoding="async"
+          className="w-auto h-full pointer-events-none object-cover"
+        />
+      </DialogTrigger>
+      <DialogContent className="md:min-w-[720px] lg:min-w-[968px] xl:min-w-[1080px]">
+          <DialogHeader>
+            <DialogTitle>{capture.data.name}</DialogTitle>
+          </DialogHeader>
+          <img
+            src={url}
+            alt=""
+            decoding="async"
+            className="w-auto h-full pointer-events-none object-cover"
+          />
+      </DialogContent>
+    </Dialog>
   );
 }
