0a63b0d2cb582af37838282a1c8917d0d88238db
mktbsh <t93.hasebe@gmail.com>
2024-05-05 12:51:37 +0900

    fix

--- src/lib/video-capture/capturer.ts
+++ src/lib/video-capture/capturer.ts
@@ -1,7 +1,7 @@
 import { createDOM } from "../dom";
 import { TransferredImage, loadVideoMetadata, transferImage } from "../video";
 import { CaptureOptions, CaptureTask, CapturedImage } from "./types";
-import { parallels, toMmSs } from "../utils";
+import { parallels } from "../utils";
 import { objectURL } from "../object-url";
 
 type InitCapturerOptions = {
@@ -23,8 +23,6 @@ const EVT_SEEKED = "seeked";
 export async function initCapturer(
   options: InitCapturerOptions,
 ): Promise<Capturer> {
-  const id = options.id;
-
   const src = objectURL.create(options.source);
 
   const video = createDOM("video", (el) => {
@@ -44,30 +42,20 @@ export async function initCapturer(
 
   let isTerminated = false;
 
-  function logger(message?: any) {
-    console.log(`[Capturer#${id}] ${message}`);
-  }
-
   function terminate() {
     if (isTerminated) return;
     const kill = (fn: VoidFunction) => new Promise((resolve) => resolve(fn()));
     objectURL.revoke(src);
     Promise.allSettled([kill(canvas.remove), kill(video.remove)]);
     isTerminated = true;
-    logger("terminated");
   }
 
   function capture({ time, output }: CaptureTask): Promise<TransferredImage> {
     return new Promise((resolve) => {
       video.currentTime = time;
-
-      const handleSeeked = async () => {
-        await transferImage({ canvas, video, output }).then(resolve);
-        logger(`captured: ${toMmSs(time)}`);
-        video.removeEventListener(EVT_SEEKED, handleSeeked);
-      };
-
-      video.addEventListener(EVT_SEEKED, handleSeeked);
+      video.addEventListener(EVT_SEEKED, () => {
+        transferImage({ canvas, video, output }).then(resolve);
+      }, { once: true });
     });
   }
 
@@ -90,8 +78,6 @@ export async function initCapturer(
   ) {
     if (isTerminated) throw new Error("Capturer is already terminated");
 
-    logger("start");
-
     const ps = parallels();
     for (const task of tasks) {
       ps.add(doCapture(task, callback));
@@ -99,10 +85,7 @@ export async function initCapturer(
     }
     await ps.all();
     terminate();
-    logger("finish");
   }
-
-  logger(`initialized: ${id}`);
 
   return {
     terminate,
